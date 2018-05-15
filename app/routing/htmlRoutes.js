7d7401e13b50d7b91aa93f4cf7f2a2356346653b
Raven Bell <raven.bell@yahoo.com>
2018-05-14 22:44:00 -0400

    route to survey.html working

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+module.exports = function(app) {
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/survey.html"));
+    });
+}
